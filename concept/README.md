71f472bb6f49278b7c4d9092383f55611095e20f
nefelibatala1993 <leuela1993@gmail.com>
2022-09-06 18:02:27 +0300

    Update README.md

--- concept/README.md
+++ concept/README.md
@@ -1,3 +1,4 @@
 <h1>Concept for the UNIX command line interpreter</h1>
 <h2>Process in UNIX</h2>
 <p>A process is an instance of an executing program. </p>
+<h3>fork()</h3>
---
+++ concept/test.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "process.h"
+
+/**
+ * main - Entry point for my program
+ *
+ * Return: On success, it returns 0.
+ * On error, it returns 1
+ */
+int main(void)
+{
+	printf("Hello World\n");
+	return (0);
+}
